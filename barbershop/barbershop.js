d1b9a717ca0f6881c5495c1158e7c20090777be9
mraziikk <lukasmraz022@gmail.com>
2026-05-27 22:11:31 +0200

    Add files via upload

---
+++ barbershop/barbershop.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navLinks = document.querySelectorAll('.nav-menu a');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('slide-down'); // Přidána třída pro animaci
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+            navMenu.classList.remove('slide-down'); // Skryjeme menu po kliknutí na odkaz
+        }
+    });
+});
